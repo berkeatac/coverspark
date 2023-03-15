85b167c68fbf40017c3ad0005660e3fa7af81f72
Berke Atac <berke.atac@gmail.com>
2023-03-16 00:37:03 +0100

    add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,15 @@ import {
   QueryClientProvider,
   QueryClient,
 } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/react";
+
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
+      <Analytics />
     </QueryClientProvider>
   );
 }
--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -1,37 +1,3 @@
-// import type { NextApiRequest, NextApiResponse } from "next";
-// const { Configuration, OpenAIApi } = require("openai");
-
-// const configuration = new Configuration({
-//   apiKey: process.env.OPENAI_API_KEY,
-// });
-
-// const openai = new OpenAIApi(configuration);
-
-// export default async function handler(
-//   _req: NextApiRequest,
-//   res: NextApiResponse
-// ) {
-//   const { name, companyName, description } = JSON.parse(_req.body);
-//   openai
-//     .createCompletion({
-//       //   model: "gpt-3.5-turbo",
-//       //   messages: [
-//       //     {
-//       //       role: "user",
-//       //       content: `I'm ${name}. I'm applying for a job at ${companyName} with a description of ${description}. Write a cover letter for me to apply at this job.`,
-//       //     },
-//       //   ],
-//       model: "text-davinci-003",
-//       max_tokens: 500,
-//       prompt: `I'm ${name}. I'm applying for a job at ${companyName} with a description of ${description}. Write me a cover letter.`,
-//     })
-//     .then((data) => {
-//       console.log(data);
-//       //   res.status(200).json({ response: data.data.choices[0].message.content });
-//       res.status(200).json({ response: data.data.choices[0].text });
-//     });
-// }
-
 import { OpenAIStream } from "../../utils/OpenAIStream";
 
 export const config = {
